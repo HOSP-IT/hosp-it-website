d5d4721af7225370f698f0ec40999c598ed223e7
HOSP-IT <dekorifyhub@gmail.com>
2026-07-09 16:30:33 +0700

    Update vite.config.ts
    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
+import vitePrerender from 'vite-plugin-prerender'
 import { defineConfig } from "vite"
 import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [inspectAttr(), react()],
+plugins: [
+      inspectAttr(), 
+      react(),
+      vitePrerender({
+        staticDir: path.resolve(__dirname, 'dist'),
+        routes: ['/', '/integrasi-satusehat'],
+      })
+    ],
   server: {
     port: 3000,
   },
